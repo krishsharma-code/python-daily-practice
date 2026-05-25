8d039015d6e4599454f7e24b8ca9d87894d67f90
krishsharma-code <krish.krishsharma9376@gmail.com>
2026-05-25 06:43:11 +0530

    Feat: Added 03_keyword_args.py for Day 5 Python Functions

--- Day_05_Functions/03_keyword_args.py
+++ Day_05_Functions/03_keyword_args.py
@@ -1,19 +1,19 @@
 # 03_keyword_args.py
-# Concept: Keyword arguments and default values
+# Concept: Keyword arguments and Default values
 
 def describe_pet(pet_name, animal_type='dog'):
     """
-    Displays information about a pet.
-    'animal_type' has a default value of 'dog'.
+    animal_type has a default value of 'dog'.
+    If no value is provided for animal_type, it uses 'dog'.
     """
     print(f"\nI have a {animal_type}.")
     print(f"My {animal_type}'s name is {pet_name.title()}.")
 
-# 1. Calling using default value for animal_type
+# Positional and default
 describe_pet(pet_name='willie')
 
-# 2. Overriding the default value
-describe_pet(pet_name='harry', animal_type='hamster')
+# Overriding the default value using positional argument
+describe_pet('harry', 'hamster')
 
-# 3. Using keyword arguments out of order
-describe_pet(animal_type='cat', pet_name='mimi')
+# Using keyword arguments (order doesn't matter here)
+describe_pet(animal_type='cat', pet_name='whiskers')
